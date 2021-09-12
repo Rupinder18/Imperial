32fe2478cc42e56ef1882ba89c5d1cfa9c2b3b18
Rupinder Kaushal <89014750+Rupinderkaushal@users.noreply.github.com>
2021-09-12 08:15:35 +0530

    mobile-view-done

--- src/Components/Community/Community.js
+++ src/Components/Community/Community.js
@@ -5,7 +5,7 @@ const Community = () => {
     return (
         <div className="com-div" style={{background:`url(${ImgBg})`,backgroundPosition:"center center",backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundAttachment:"fixed"}}>
             <h1>COMMUNITY</h1>
-            <p style={{marginBottom:"20px"}}>If you’d like to donate to the Devs, send ETH, ICT, VADR or DROID here. Thank you for your support!</p>
+            <p id='p2' >If you’d like to donate to the Devs, send ETH, ICT, VADR or DROID here. Thank you for your support!</p>
             <p>Our community keeps growing everyday. Please join our social platforms to get updates using the links below.<br/>
 <br/>
 There are 150k+ people in our different groups!</p>
